4cde9bf76b487fcc344597046de797c616349a1b
simon <simonhong85@gmail.com>
2023-08-19 21:19:34 +0200

    add config for deploy

--- mrs-backend/src/main/java/org/mrs/teamapi/TeamAPI.java
+++ mrs-backend/src/main/java/org/mrs/teamapi/TeamAPI.java
@@ -3,15 +3,13 @@ package org.mrs.teamapi;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-@EnableMongoRepositories(basePackages = {
-        "org.mrs.teamapi.employee", "org.mrs.teamapi.team", "org.mrs.teamapi.login"
-})
 public class TeamAPI {
     private static final Logger log = LoggerFactory.getLogger(TeamAPI.class);
     public static void main(String[] args) {
